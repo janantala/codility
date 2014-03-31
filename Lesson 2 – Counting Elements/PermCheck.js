f1b5ad9cbc5ce5bfa388b244f6187b2e95f4047e
Jan Antala <jan@antala.sk>
2014-03-31 14:42:42 +0200

    refactor: use object in PermCheck task

--- Lesson 2 – Counting Elements/PermCheck.js
+++ Lesson 2 – Counting Elements/PermCheck.js
@@ -2,7 +2,7 @@
 
 module.exports = function solution(A) {
   
-  var P = new Array(A.length + 1);
+  var P = {};
 
   for (var i=0; i<A.length; i++){
     if (A[i] > A.length){
@@ -12,11 +12,5 @@ module.exports = function solution(A) {
     P[A[i]] = true;
   }
 
-  for (var j=1; j<P.length; j++){
-    if (!P[j]){
-      return 0;
-    }
-  }
-
-  return 1;
+  return Object.keys(P).length === A.length ? 1 : 0;
 };
