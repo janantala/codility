8abe18414ed30ea6d77aff4429d13ea077365269
Jan Antala <jan@antala.sk>
2014-04-03 23:36:22 +0200

    test: fix Distinct test spec

--- Lesson 4 – Sorting/Distinct.spec.js
+++ Lesson 4 – Sorting/Distinct.spec.js
@@ -2,7 +2,7 @@
 var assert = require('assert');
 var solution = require('./Distinct');
 
-it('should return the number of passing cars', function() {
+it('should return the number of distinct numbers', function() {
   assert.deepEqual(solution([2,1,1,2,3,1]), 3);
   assert.deepEqual(solution([2,1,1,2,3,1,-1,-1]), 4);
 });
